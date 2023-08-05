5e3760e9f30c8b4a997db05d0810c4f02f7a6977
pelmenman <akula_from_ikea@mail.ru>
2023-08-05 21:11:32 +0300

    Theme is changed

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tictactoe.ui.theme.TicTacToeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TicTacToeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Tic Tac Toe!")
+        Button(onClick = {},
+            modifier = Modifier.padding(top = 50.dp, bottom = 20.dp)){
+            Text("Single-player")
+        }
+        Button(onClick = {}, modifier = Modifier.padding(20.dp)){Text("Multiplayer")}
+        Button(onClick = {}, modifier = Modifier.padding(20.dp)){Text("Settings")}
+    }
+
+}
---
+++ app/src/main/java/com/example/tictactoe/ui/theme/Color.kt
@@ -0,0 +1,38 @@
+package com.example.tictactoe.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBlue90 = Color(0xFFe8f4fc)
+val LightBlue80 = Color(0xFFa3d3f5)
+val LightBlue70 = Color(0xFF75bdf0)
+val LightBlue65 = Color(0xFF5eb2ed)
+val LightBlue60 = Color(0xFF47a7eb)
+val LightBlue50 = Color(0xFF1991e6)
+val LightBlue40 = Color(0xFF1474b8)
+val LightBlue30 = Color(0xFF0f578a)
+val LightBlue20 = Color(0xFF0a3a5c)
+val LightBlue10 = Color(0xFF051d2e)
+
+val Grey90 = Color(0xFFe6e6e6)
+val Grey80 = Color(0xFFcccccc)
+val Grey70 = Color(0xFFb3b3b3)
+val Grey60 = Color(0xFF999999)
+val Grey57 = Color(0xFF929292)
+val Grey50 = Color(0xFF808080)
+val Grey40 = Color(0xFF666666)
+val Grey30 = Color(0xFF4d4d4d)
+val Grey20 = Color(0xFF333333)
+val Grey10 = Color(0xFF1a1a1a)
+
+
+
+val PinkyRed90 = Color(0xFFfdced7)
+val PinkyRed80 = Color(0xFFfa9eaf)
+val PinkyRed70 = Color(0xFFf86d86)
+val PinkyRed62 = Color(0xFFF64766)
+val PinkyRed60 = Color(0xFFf63c5e)
+val PinkyRed50 = Color(0xFFf40b36)
+val PinkyRed40 = Color(0xFFc3092b)
+val PinkyRed30 = Color(0xFF920720)
+val PinkyRed20 = Color(0xFF610516)
+val PinkyRed10 = Color(0xFF31020b)
